654248d18eb74f8fbd2e133783e05481d6135ef5
Bojan Gluvacevic <38956086+BojanGluvacevic@users.noreply.github.com>
2021-08-01 16:25:42 +0100

    hg
    jh

---
+++ BojanGluvacevic.github.io/README.md
@@ -0,0 +1,1 @@
+# BojanGluvacevic.github.io
---
+++ BojanGluvacevic.github.io/assets/js/core.js
@@ -0,0 +1,150 @@
+/* global bootstrap: false */
+
+(function () {
+  'use strict'
+
+  // Tooltip and popover demos
+  document.querySelectorAll('.tooltip-demo')
+    .forEach(function (tooltip) {
+      new bootstrap.Tooltip(tooltip, {
+        selector: '[data-bs-toggle="tooltip"]'
+      })
+    })
+
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+    .forEach(function (popover) {
+      new bootstrap.Popover(popover)
+    })
+
+  document.querySelectorAll('.toast')
+    .forEach(function (toastNode) {
+      var toast = new bootstrap.Toast(toastNode, {
+        autohide: false
+      })
+
+      toast.show()
+    })
+
+  // Disable empty links
+  document.querySelectorAll('[href="#"]')
+    .forEach(function (link) {
+      link.addEventListener('click', function (event) {
+        event.preventDefault()
+      })
+    })
+
+    
+  function setActiveItem() {
+    var hash = window.location.hash
+
+    if (hash === '') {
+      return
+    }
+
+    var link = document.querySelector('.bd-aside a[href="' + hash + '"]')
+    var active = document.querySelector('.bd-aside .active')
+    var parent = link.parentNode.parentNode.previousElementSibling
+
+    link.classList.add('active')
+
+    if (parent.classList.contains('collapsed')) {
+      parent.click()
+    }
+
+    if (!active) {
+      return
+    }
+
+    var expanded = active.parentNode.parentNode.previousElementSibling
+
+    active.classList.remove('active')
+
+    if (expanded && parent !== expanded) {
+      expanded.click()
+    }
+  }
+/*
+  setActiveItem()
+    window.addEventListener('hashchange', setActiveItem)
+*/
+})()
+
+function MLG__Prepare(name, to, subject, body, outputControl) {
+    var templateName = document.getElementById(name).value;
+    var mailTo = document.getElementById(to).value;
+    var mailSubject = document.getElementById(subject).value;
+    var mailBody = document.getElementById(body).value;
+
+    //encode values
+    mailTo = "&to=" + encodeURIComponent(mailTo);
+    mailSubject = "&subject=" + encodeURIComponent(mailSubject);
+    mailBody = "&body=" + encodeURIComponent(mailBody);
+
+    var mailBaseURL = "https://outlook.office.com/?path=/mail/action/compose";
+    var mailNewLink = mailBaseURL + mailTo + mailSubject + mailBody;
+
+    document.getElementById(outputControl).value = mailNewLink;
+    $("#mlg__URLOpen").attr("href", mailNewLink);
+}
+
+function copyToClipboard(input) {
+    var copyText = document.getElementById(input).value;
+    navigator.clipboard.writeText(copyText);
+}
+
+function MLG_Table_Add(name, to, subject, body) {
+    if ($("#mlg-table tbody").length == 0) {
+        $("#mlg-table").append("<tbody></tbody>");
+    }
+
+    if ($("#mlg__name").val() != null && $("#mlg__name").val() != '') {
+
+        $("#mlg-table tbody").append("<tr>" +
+            "<td>" + document.getElementById(name).value + "</td>" +
+            "<td>" + document.getElementById(to).value + "</td>" +
+            "<td>" + document.getElementById(subject).value + "</td>" +
+            "<td>" + document.getElementById(body).value.replace(/(?:\r|\n|\r\n)/g, '<br>') + "</td>" +
+            "<td>" + "<button type='button' onclick=\"$(this).closest('tr').remove()\" class='btn btn-default'>" + "<i class='mdi mdi-delete'>" + "</button>" + "</td>" +
+            "</tr>");
+    }
+}
+
+window.actionEvents = {
+    'click .up': function (e, value, row, index) {
+        var source = JSON.stringify($('#table').bootstrapTable('getData')[index]);
+        var target = JSON.stringify($('#table').bootstrapTable('getData')[index - 1]);
+        $('#table').bootstrapTable('updateRow', { 'index': index - 1, 'row': JSON.parse(source) });
+        $('#table').bootstrapTable('updateRow', { 'index': index, 'row': JSON.parse(target) });
+    },
+    'click .down': function (e, value, row, index) {
+        var source = JSON.stringify($('#table').bootstrapTable('getData')[index]);
+        var target = JSON.stringify($('#table').bootstrapTable('getData')[index + 1]);
+        $('#table').bootstrapTable('updateRow', { 'index': index + 1, 'row': JSON.parse(source) });
+        $('#table').bootstrapTable('updateRow', { 'index': index, 'row': JSON.parse(target) });
+    }
+}
+
+var importSettings = function (event) {
+    var input = event.target;
+
+    var reader = new FileReader();
+    reader.onload = function () {
+        var text = reader.result;
+         
+        console.log('import settings');
+    };
+    reader.readAsText(input.files[0]);
+};
+
+var exportSettings = function (event) {
+    var input = event.target;
+
+    var reader = new FileReader();
+    reader.onload = function () {
+        var text = reader.result;
+
+        console.log('export settings');
+    };
+    reader.readAsText(input.files[0]);
+};
+

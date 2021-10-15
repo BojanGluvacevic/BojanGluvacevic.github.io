faa167e25ec3c2cff4c1300744ce419e84b27f37
Bojan Gluvacevic <38956086+BojanGluvacevic@users.noreply.github.com>
2021-10-15 21:05:24 +0100

    First working version

--- assets/js/core.js
+++ assets/js/core.js
@@ -87,23 +87,85 @@ function MLG__Prepare(name, to, subject, body, outputControl) {
     $("#mlg__URLOpen").attr("href", mailNewLink);
 }
 
+function prepareURL_OWA(to, subject, body) {
+    //encode values
+    var mailTo = "&to=" + encodeURIComponent(to);
+    var mailSubject = "&subject=" + encodeURIComponent(subject);
+    var mailBody = "&body=" + encodeURIComponent(body);
+
+    var mailBaseURL = "https://outlook.office.com/?path=/mail/action/compose";
+    return (mailBaseURL + mailTo + mailSubject + mailBody);
+}
+
+function prepareURL_App(to, subject, body) {
+    var mailTo = "mailto:" + encodeURIComponent(to);
+    var mailSubject = "&subject=" + encodeURIComponent(subject);
+    var mailBody = "&body=" + encodeURIComponent(body);
+
+    return (mailTo + mailSubject + mailBody);
+}
+
 function copyToClipboard(input) {
     var copyText = document.getElementById(input).value;
     navigator.clipboard.writeText(copyText);
 }
 
-function MLG_Table_Add(id, name, to, subject, body, url, uri) {
+var generateRandomNDigits = (n) => {
+    return Math.floor(Math.random() * (9 * (Math.pow(10, n-1)))) + (Math.pow(10, n-1));
+}
+
+
+function deleteWithPrompt(id) {
+    if (confirm('Do you really want to delete this entry?')) {
+        $('#' + id).remove();  //remove the element
+    }
+    else {
+        alert('Watch where you press!');
+    }
+}
+
+function MLG_Table_Add(name, to, subject, body, url, uri) {
     if ($("#mlg-table tbody").length == 0) {
         $("#mlg-table").append("<tbody></tbody>");
     }
 
-    $("#mlg-table tbody").append("<tr>" +
-        "<td>" + name + "</td>" +
-        "<td>" + to + "</td>" +
-        "<td>" + subject + "</td>" +
-        "<td>" + body + "</td>" +
-        "<td>" + "<button type='button' onclick=\"$(this).closest('tr').remove()\" class='btn btn-default'>" + "<i class='mdi mdi-delete'>" + "</button>" + "</td>" +
+    //read name from the browser
+    var r_name = document.getElementById(name).value;
+    var r_to = document.getElementById(to).value;
+    var r_subject = document.getElementById(subject).value;
+    var r_body = document.getElementById(body).value;
+
+    var r_url_owa = prepareURL_OWA(r_to, r_subject, r_body);
+    var r_url_app = prepareURL_App(r_to, r_subject, r_body);
+
+    var rand_id = generateRandomNDigits(8);
+
+    $("#mlg-table tbody").append("<tr id='" + rand_id + "'>" +
+        "<td>" + r_name + "</td>" +
+        "<td>" +
+        "   <button type='button' onclick=\"navigator.clipboard.writeText('" + r_url_owa + "');\" class='btn btn-default' title='Copy Mailto link for OWA'><i class='mdi mdi-content-copy ' /></button>" +
+        "   <button type='button' onclick=\"navigator.clipboard.writeText('" + r_url_app + "');\" class='btn btn-default' title='Copy Mailto link for Applications'><i class='mdi mdi-application' /></button>" +
+        "   <button type='button' onclick=\"window.open('" + r_url_owa  + "', '_blank');\" class='btn btn-default' title='Open in new window'><i class='mdi mdi-open-in-new' /></button>" +
+                "   <button type='button' onclick=\"deleteWithPrompt(" + rand_id + ")\" class='btn btn-default' title='Delete record'><i class='mdi mdi-delete' /></button>" +
+        "</td>" +
         "</tr>");
+}
+
+function MLG__Control(id) {
+    var url = document.getElementById(id).value;
+
+    if (url.length > 13) { // don't fire on any change, need to have some input first
+        var myregexp = /^(https:\/\/outlook\.office\.com\/\?path=\/mail\/action\/compose&to=|mailto:)(.*?)&subject=(.*?)&body=(.*?)$/img;
+        var match = myregexp.exec(url);
+        if (match != null) {
+            //show recipients
+            document.getElementById('mlg__control__to').value = unescape(match[2]);
+            //show subject
+            document.getElementById('mlg__control__subject').value = unescape(match[3]);
+            //show message
+            document.getElementById('mlg__control__message').value = unescape(match[4]);
+        }
+    }
 }
 
 var openFile = function (event) {
